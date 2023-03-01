f9786d5caa884200e86e59753d6de1c33115781b
christian ng'ang'a <111011919+themathematcs@users.noreply.github.com>
2023-03-01 21:29:44 +0300

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,9 +9,11 @@
  * Return: A pointer to the lowest common ancestor node of the two given nodes.
  *         If no common ancestor was found, your function must return NULL.
  */
+
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
+	
 	binary_tree_t *mom, *pop;
 
 	if (!first || !second)

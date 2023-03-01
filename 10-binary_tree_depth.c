58d9fa0f83566ff0900c3f4b3c3c6ca68fe9bbcd
christian ng'ang'a <111011919+themathematcs@users.noreply.github.com>
2023-03-01 21:29:22 +0300

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,7 +7,12 @@
  *
  * Return: 0 if tree is NULL.
  */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	
+	
 	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	
+	
 }
